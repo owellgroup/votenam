88d9e0185d385ff8c32052b2622040ecb9cd12cf
Joel-FutureMedia <joel.kalimbwe@futuremedia.com.na>
2025-11-20 13:40:36 +0200

    d

---
+++ src/main/java/com/example/votenam/VotenamApplication.java
@@ -0,0 +1,13 @@
+package com.example.votenam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotenamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotenamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/votenam/VotenamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.votenam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotenamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
